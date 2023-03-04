d09f4c89f62c55c48ee34da8157e4d531ad7e44d
sasha_kr <a.krutseniuk@gmail.com>
2023-03-04 08:09:37 +0200

    commit

--- main.tf
+++ main.tf
@@ -62,7 +62,7 @@ resource "aws_route_table_association" "public_subnet_association" {
 # Создание приватной подсети
 resource "aws_subnet" "private_subnet" {
   vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.0.2.0/24"
   #availability_zone = "us-east-1a"
   tags = {
     Name = "Private-Subnet"
