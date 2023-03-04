e108b65d705b7745573d3ebf929c44b2b962b322
sasha_kr <a.krutseniuk@gmail.com>
2023-03-04 07:59:47 +0200

    commit

--- main.tf
+++ main.tf
@@ -96,7 +96,7 @@ resource "aws_nat_gateway" "my_nat" {
   allocation_id = aws_eip.my_eip.id
   subnet_id     = aws_subnet.public_subnet.id
 
-  depends_on = [aws_internet_gateway_attachment.my_igw_attachment]
+ # depends_on = [aws_internet_gateway_attachment.my_igw_attachment]
 }
 
 
