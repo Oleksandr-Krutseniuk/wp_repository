e5fe05eebd7a66a3f162240da1c386d68c1085db
sasha_kr <a.krutseniuk@gmail.com>
2023-03-05 14:21:59 +0200

    commit

--- main.tf
+++ main.tf
@@ -293,10 +293,10 @@ resource "aws_security_group" "allow_http_for_ec2" {
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "TCP"
+    cidr_blocks = [aws_subnet.public_subnet.cidr_block]
   }
 
   vpc_id = aws_vpc.my_vpc.id
