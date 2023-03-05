e6af97fc04d15f38cc6fd60292a0358ef96ced3f
sasha_kr <a.krutseniuk@gmail.com>
2023-03-05 13:57:03 +0200

    commit

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ resource "aws_subnet" "public_subnet" {
 resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.my_vpc.id
   route {
-    destination_cidr_block = "0.0.0.0/0"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.my_igw.id
   }
   tags = {
