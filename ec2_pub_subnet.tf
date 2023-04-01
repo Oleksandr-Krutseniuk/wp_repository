4a5fa79098b4b9b64790ece7315c3ce19597eac7
sasha_kr <a.krutseniuk@gmail.com>
2023-04-01 08:01:38 +0300

    added ports to test EC2

--- ec2_pub_subnet.tf
+++ ec2_pub_subnet.tf
@@ -77,7 +77,7 @@ resource "aws_route_table" "public_route_table_2" {
 }
 
 # Attach public subnet1 to route table (makes route table active within a subnet)
-resource "aws_route_table_association" "public_subnet_association_1" {
+resource "aws_route_table_association" "public_subnet_association_2" {
   subnet_id      = aws_subnet.public_subnet_2.id
   route_table_id = aws_route_table.public_route_table_2.id
 }
