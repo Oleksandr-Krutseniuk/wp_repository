eab78fb6c0917b1bb54af0496a17e096939a13eb
sasha_kr <a.krutseniuk@gmail.com>
2023-05-06 07:09:03 +0300

    changed egress rules

--- ec2_pub_subnet.tf
+++ ec2_pub_subnet.tf
@@ -108,28 +108,13 @@ resource "aws_security_group" "bastion_sg" {
       cidr_blocks = ["0.0.0.0/0"]
      } 
      
-    egress {
-      from_port        = 443
-      to_port          = 443
-      protocol         = "tcp"  
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-
     ingress {
       from_port   = 80
       to_port     = 80
       protocol    = "tcp"  
       cidr_blocks = ["0.0.0.0/0"]
      } 
-     
-    egress {
-      from_port        = 80
-      to_port          = 80
-      protocol         = "tcp"  
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  
-
+    
     ingress {
       from_port   = 8080
       to_port     = 8080
@@ -137,14 +122,6 @@ resource "aws_security_group" "bastion_sg" {
       cidr_blocks = ["0.0.0.0/0"]
      } 
      
-    egress {
-      from_port        = 8080
-      to_port          = 8080
-      protocol         = "tcp"  
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-
-
     ingress { 
       from_port   = 22 
       to_port     = 22
@@ -152,12 +129,13 @@ resource "aws_security_group" "bastion_sg" {
       cidr_blocks = ["0.0.0.0/0"]
     }  
     
-    egress { 
-      from_port   = 22 
-      to_port     = 22
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    } 
+    egress {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "tcp"  
+      cidr_blocks      = ["0.0.0.0/0"]
+    }
+
   }
 
 
